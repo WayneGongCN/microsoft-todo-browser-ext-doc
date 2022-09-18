aaebbdba3a118473d47def10a882b8969fc99272
wayne <me@waynegong.cn>
2022-09-18 14:32:31 +0800

    chore: update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import Translate, { translate } from '@docusaurus/Translate';
+import Translate from '@docusaurus/Translate';
 import styles from './index.module.css';
+import Link from '@docusaurus/Link';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title"><Translate>{siteConfig.title}</Translate></h1>
-        <p className="hero__subtitle"><Translate>{siteConfig.tagline}</Translate></p>
-        {/* <div className={styles.buttons}>
+        <h1 className="hero__title"><Translate>Microsoft To Do 浏览器扩展</Translate></h1>
+        <p className="hero__subtitle"><Translate>To Do 让你从工作到娱乐都保持专注</Translate></p>
+        <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="./docs/intro/install">
+            <Translate>安装</Translate>
           </Link>
-        </div> */}
+        </div>
       </div>
     </header>
   );
